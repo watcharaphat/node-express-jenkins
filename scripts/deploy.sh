f1d85ad348b0028ee73f1cee8c52299c8d24e76f
watcharaphat <watcharaphat.3166@gmail.com>
2018-06-22 16:59:44 +0700

    edit deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
+set -x
 npm run build
-
-if ! [ -x "$(command -v pm2)" ]; then
-  npm install -g pm2
-fi
+set +x
 
 set -x
-pm2-runtime build/server.js &
+npm start &
+sleep 1
+
+echo $! > .pidfile
+set +x
